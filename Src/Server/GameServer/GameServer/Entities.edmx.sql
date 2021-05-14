42fa5c3a522793ce990082cb0c0c80081078cd6f
ShyHeart <18296998265@163.com>
2021-05-14 17:36:40 +0800

    数据库生成 协议生成

--- Src/Server/GameServer/GameServer/Entities.edmx.sql
+++ Src/Server/GameServer/GameServer/Entities.edmx.sql
@@ -2,8 +2,8 @@
 -- --------------------------------------------------
 -- Entity Designer DDL Script for SQL Server 2005, 2008, 2012 and Azure
 -- --------------------------------------------------
--- Date Created: 09/07/2018 00:02:53
--- Generated from EDMX file: D:\Projects\ExtremeWorld\project-z\Src\Server\GameServer\GameServer\Entities.edmx
+-- Date Created: 05/14/2021 17:07:24
+-- Generated from EDMX file: F:\Github Depot\Ultra World\Src\Server\GameServer\GameServer\Entities.edmx
 -- --------------------------------------------------
 
 SET QUOTED_IDENTIFIER OFF;
